77baf0e326f5a1a1ef9ebe49fc9f36767f35fcf2
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-10-16 01:47:13 +0000

    AppBar created

--- components/AppBar.js
+++ components/AppBar.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import styled from "styled-components/native";
 
+import { Feather, MaterialCommunityIcons} from "@expo/vector-icons";
+
+
 const Container = styled.View`
         width: 100%;
         height: 58px;
@@ -13,10 +16,35 @@ const Container = styled.View`
 
 const Text = styled.Text `
     color:#3a86e9;
+    font-size:25px;
+    font-weight:bold;
+    letter-spacing:-0.3px;
+`
+const Row = styled.View`
+flex-direction:row;
+`
+const Button = styled.TouchableOpacity`
+  width:42px;
+  height: 42px;
+  border-radius:21px;
+  background:#EEEEEE;
+  justify-content:center;
+  align-items:center;
+  margin-left:16px;
 `
 function AppBar() {
     return (
         <Container>
+            <Text>facebook</Text>
+            <Row>
+                <Button>
+                    <Feather name="search" size={23} color="black"/>
+                </Button>
+
+                <Button>
+                    <MaterialCommunityIcons name="facebook-messenger" size={23} color="black" />
+                </Button>
+            </Row>
 
         </Container>
     )
